9846292132d4f6455631eb5580ba48d84bc8b102
weicht <chris.weicht@gmail.com>
2017-09-09 19:52:44 -0400

    Initial Commit

---
+++ LaunchTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LaunchTest
+//
+//  Created by Chris Weicht on 9/9/17.
+//  Copyright © 2017 Chris Weicht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
